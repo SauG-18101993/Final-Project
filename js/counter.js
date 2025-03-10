f2170720cd9255b9b6f57715902b343b54dc9ef6
SauG-18101993 <you@example.com>
2025-03-11 00:45:46 +0530

    fifteen commit

--- js/counter.js
+++ js/counter.js
@@ -184,5 +184,9 @@ $('button').click(function(){
             });
         });
     });
+    document.getElementById("sign-up").addEventListener("click", () => {
+        // Redirect to the signup.html page
+        window.location.href = "signup.html";
+    });
 
     
---
+++ js/login.js
@@ -0,0 +1,4 @@
+document.getElementById("Sign-up").addEventListener("click", () => {
+    // Redirect to the signup.html page
+    window.location.href = "signup.html";
+});
